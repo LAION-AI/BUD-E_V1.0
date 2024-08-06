239a12ae15b2faaa171fdb4d215c08b29247ca6c
Christoph Schuhmann <christoph_s@freenet.de>
2024-08-06 18:19:18 +0200

    Update stylettsserver.py

--- stylettsserver.py
+++ stylettsserver.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse, FileResponse
+from pydantic import BaseModel
 import threading
 import time
 import json
-import os  # Import the os module
-
+import os
+import random
+import soundfile as sf
 
 import torch
 torch.manual_seed(0)
@@ -18,27 +21,24 @@ np.random.seed(0)
 
 import nltk
 nltk.download('punkt')
-import os
 import soundfile as sf
-# load packages
 import time
-import random
 import yaml
 from munch import Munch
-import numpy as np
 import torch
 from torch import nn
 import torch.nn.functional as F
 import torchaudio
 import librosa
 from nltk.tokenize import word_tokenize
-
 from models import *
 from utils import *
 from text_utils import TextCleaner
-textclenaer = TextCleaner()
+import phonemizer
+from Utils.PLBERT.util import load_plbert
+from Modules.diffusion.sampler import DiffusionSampler, ADPM2Sampler, KarrasSchedule
 
-# %matplotlib inline
+textclenaer = TextCleaner()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -73,7 +73,6 @@ def compute_style(ref_dicts):
     return reference_embeddings
 
 # load phonemizer
-import phonemizer
 global_phonemizer = phonemizer.backend.EspeakBackend(language='en-us', preserve_punctuation=True, with_stress=True, words_mismatch='ignore')
 
 config = yaml.safe_load(open("Models/LJSpeech/config.yml"))
@@ -88,7 +87,6 @@ F0_path = config.get('F0_path', False)
 pitch_extractor = load_F0_models(F0_path)
 
 # load BERT model
-from Utils.PLBERT.util import load_plbert
 BERT_path = config.get('PLBERT_dir', False)
 plbert = load_plbert(BERT_path)
 
@@ -113,11 +111,8 @@ for key in model:
                 new_state_dict[name] = v
             # load params
             model[key].load_state_dict(new_state_dict, strict=False)
-#             except:
-#                 _load(params[key], model[key])
-_ = [model[key].eval() for key in model]
 
-from Modules.diffusion.sampler import DiffusionSampler, ADPM2Sampler, KarrasSchedule
+_ = [model[key].eval() for key in model]
 
 sampler = DiffusionSampler(
     model.diffusion.diffusion,
@@ -175,115 +170,25 @@ def inference(text, noise, diffusion_steps=5, embedding_scale=1):
 
     return out.squeeze().cpu().numpy()
 
-def LFinference(text, s_prev, noise, alpha=0.7, diffusion_steps=5, embedding_scale=1):
-  text = text.strip()
-  text = text.replace('"', '')
-  ps = global_phonemizer.phonemize([text])
-  ps = word_tokenize(ps[0])
-  ps = ' '.join(ps)
+app = FastAPI()
 
-  tokens = textclenaer(ps)
-  tokens.insert(0, 0)
-  tokens = torch.LongTensor(tokens).to(device).unsqueeze(0)
+class TextInput(BaseModel):
+    text: str
 
-  with torch.no_grad():
-      input_lengths = torch.LongTensor([tokens.shape[-1]]).to(tokens.device)
-      text_mask = length_to_mask(input_lengths).to(tokens.device)
-
-      t_en = model.text_encoder(tokens, input_lengths, text_mask)
-      bert_dur = model.bert(tokens, attention_mask=(~text_mask).int())
-      d_en = model.bert_encoder(bert_dur).transpose(-1, -2)
-
-      s_pred = sampler(noise,
-            embedding=bert_dur[0].unsqueeze(0), num_steps=diffusion_steps,
-            embedding_scale=embedding_scale).squeeze(0)
-
-      if s_prev is not None:
-          # convex combination of previous and current style
-          s_pred = alpha * s_prev + (1 - alpha) * s_pred
-
-      s = s_pred[:, 128:]
-      ref = s_pred[:, :128]
-
-      d = model.predictor.text_encoder(d_en, s, input_lengths, text_mask)
-
-      x, _ = model.predictor.lstm(d)
-      duration = model.predictor.duration_proj(x)
-      duration = torch.sigmoid(duration).sum(axis=-1)
-      pred_dur = torch.round(duration.squeeze()).clamp(min=1)
-
-      pred_aln_trg = torch.zeros(input_lengths, int(pred_dur.sum().data))
-      c_frame = 0
-      for i in range(pred_aln_trg.size(0)):
-          pred_aln_trg[i, c_frame:c_frame + int(pred_dur[i].data)] = 1
-          c_frame += int(pred_dur[i].data)
-
-      # encode prosody
-      en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(device))
-      F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-      out = model.decoder((t_en @ pred_aln_trg.unsqueeze(0).to(device)),
-                              F0_pred, N_pred, ref.squeeze().unsqueeze(0))
-
-  return out.squeeze().cpu().numpy(), s_pred
-
-
-
-
-
-app = Flask(__name__)
-filename = 'ytitems.json'  # The filename in which the list will be saved
-
-# Initialize the items list
-items = ["47000","51800", "18000","74400"]
-
-def load_items_from_file():
-    """Loads the list from a JSON file if it exists."""
-    if os.path.exists(filename):  # Check if the file exists
-        try:
-            with open(filename, 'r') as file:
-                global items
-                items = json.load(file)  # Load the list from the file
-        except json.JSONDecodeError:
-            print("Error decoding JSON from file.")
-        except Exception as e:
-            print(f"An error occurred while loading the list: {e}")
-
-def save_items_to_file():
-    """Regularly saves the list in a JSON file."""
-    while True:
-        try:
-            with open(filename, 'w') as file:
-                json.dump(items, file)
-            time.sleep(60)  # Saves the list every 60 seconds
-        except Exception as e:
-            print(f"An error occurred while saving the list: {e}")
-
-@app.route('/add', methods=['GET'])
-def add_item():
-    """Adds an item to the list received via a GET request."""
-    item = request.args.get('item')
-    if item:
-        items.append(item)
-        return jsonify(success=True, message="Item added successfully."), 200
-    else:
-        return jsonify(success=False, message="No item provided."), 400
-
-@app.route('/items', methods=['GET'])
-def get_items():
-    """Returns the current list."""
-    return jsonify(items), 200
-
-@app.route('/synthesize', methods=['POST'])
-def synthesize_speech():
-    text = request.json.get('text')
+@app.post("/synthesize")
+async def synthesize_speech(text_input: TextInput):
+    text = text_input.text
+    print("received request:", text)
     if not text:
-        return jsonify(success=False, message="No text provided."), 400
+        raise HTTPException(status_code=400, detail="No text provided.")
 
     try:
         # Generate speech
-        noise = torch.randn(1, 1, 192).to(device)
-        audio = inference(text, noise)
-
+        noise = torch.randn(1,1,256).to(device)
+        s = time.time()
+        audio = inference(text, noise, diffusion_steps=3, embedding_scale=1.2)
+        print("Time for inference:", time.time()-s)
+        
         # Generate a random filename
         random_number = random.randint(10000, 99999)
         filename = f"speech_{random_number}.wav"
@@ -294,18 +199,22 @@ def synthesize_speech():
         # Save the audio file
         sf.write(os.path.join('audio', filename), audio, 24000)
 
-        return jsonify(success=True, filename=filename), 200
+        return JSONResponse(content={"success": True, "filename": filename}, status_code=200)
+
     except Exception as e:
-        return jsonify(success=False, message=f"An error occurred: {str(e)}"), 500
-        
+        return JSONResponse(content={"success": False, "message": f"An error occurred: {str(e)}"}, status_code=500)
+
+@app.get("/audio/{filename}")
+async def serve_audio(filename: str):
+    file_path = os.path.join('audio', filename)
+    if os.path.exists(file_path):
+        return FileResponse(file_path)
+    else:
+        raise HTTPException(status_code=404, detail="Audio file not found")
 
 if __name__ == '__main__':
-    print(items)
-    load_items_from_file()  # Load the list from file at startup
-    print(items)
-    # Starts the thread that regularly saves the list
-    threading.Thread(target=save_items_to_file, daemon=True).start()
-    # Starts the Flask web server
-    app.run(host='0.0.0.0', port=5000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5001)
 
-
+# Uncomment the following line if you want to print the public IP
+# os.system("curl ifconfig.me")
